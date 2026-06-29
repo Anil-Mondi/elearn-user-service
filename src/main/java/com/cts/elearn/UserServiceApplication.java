d2d50653e53975a1f54b568d2f82aeae536dad11
anilmondi <4188manil@gmail.com>
2026-06-29 22:21:32 +0530

    feat: enable service discovery and Feign clients in UserServiceApplication

--- src/main/java/com/cts/elearn/UserServiceApplication.java
+++ src/main/java/com/cts/elearn/UserServiceApplication.java
@@ -2,10 +2,14 @@ package com.cts.elearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.cts.elearn.repository")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
